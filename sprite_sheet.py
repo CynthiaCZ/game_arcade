c63d13c88df067f4abb34bc5ed7d43cd104889f8
CynthiaCZ <chengyue_zhang@brown.edu>
2021-04-07 16:26:38 -0400

    Sprite animation
    Frame by frame animation using a sprite sheet

---
+++ sprite_sheet.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+# divide the sprite sheet into 4*4 tile
+im = Image.open("sprite_sheet2/sprite2.png")
+imgwidth = im.size[0]/4
+imgheight = im.size[1]/4
+# name each frame with direction + frame number
+keyList = ["D1", "D2", "D3", "D4", "L1", "L2", "L3", "L4", "R1", "R2", "R3", "R4", "U1", "U2", "U3", "U4"]
+# create a dictionary
+k = 0
+for j in range(4):
+    for i in range(4):
+        im_crop = im.crop((i*imgwidth,j*imgheight,(i+1)*imgwidth,(j+1)*imgheight))
+        k += 1
+        key = keyList[k-1]
+        d[key] = im_crop
+        im_crop.save('sprite_sheet2/' + key + '.png')
+
+# convert the dictionary into variables
+for key, val in d.items():
+        exec(key + '=val')
