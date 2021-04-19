8698c3581960cc6ef80ea4fd2dd2babf48896214
spender29 <79882373+spender29@users.noreply.github.com>
2021-04-19 18:31:42 -0400

    Added piece move() method

--- Tetris.py
+++ Tetris.py
@@ -1,3 +1,5 @@
+# Added piece move() method
+
 import pygame, sys
 #Importing key inputs that will be used for sprite navigation
 from pygame.locals import (
@@ -11,6 +13,12 @@ from pygame.locals import (
     QUIT,
 )
 
+def drawGrid(gridWidth,gridHeight,blockSize,color,surface):
+    for x in range(0, gridWidth, blockSize):
+        for y in range(0, gridHeight, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(surface, color, rect, 1)
+
 class block(pygame.sprite.Sprite):
     def __init__(self, color, x, y, w, h):
         super().__init__() #Calling parent class
@@ -20,6 +28,30 @@ class block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x*blockSize
         self.rect.y = y*blockSize
+
+    def move(self,direc,mag):
+        if (direc == 'L'):
+            self.rect.move_ip(-mag,0)
+        elif (direc =='R'):
+            self.rect.move_ip(mag,0)
+        elif (direc == 'D'):
+            self.rect.move_ip(0,mag)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect) 
+
+class piece(pygame.sprite.Group):
+    def __init__(self,color,x,y,shape):
+        super().__init__()
+        self.shape = shape
+        self.color = color
+        for i in range(4):
+            self.add(block(color,x+i,y,1,1)) #drawing I piece
+
+    def move(self,direc):
+        for blk in self.sprites():
+            mag = 1
+            blk.move(direc,mag)
             
 # Initialize program
 pygame.init()
@@ -51,6 +83,10 @@ fullspritelist = pygame.sprite.Group()
 fullspritelist.add(testBlock)
 fullspritelist.draw(DISPLAYSURF)
 
+# testing out piece class
+testPiece = piece(BLUE,3,3,'I')
+testPiece.draw(DISPLAYSURF)
+
 
 # Beginning Game Loop
 count = 0
@@ -58,9 +94,24 @@ while True:
     count += 1
     print(count) # to make sure not frozen
 
+    if count%5 == 0:
+        testBlock.move('D',1)
+        testPiece.move('D')
+        DISPLAYSURF.fill(WHITE)
+        drawGrid(gridWidth,gridHeight,blockSize,BLACK,DISPLAYSURF)
+        testPiece.draw(DISPLAYSURF)
+        testBlock.draw(DISPLAYSURF)
+
     # Necessary board updates, from pygame tutorial
     pygame.display.update()
     for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                print('LEFT')
+                testBlock.move('L',1*blockSize)
+            if event.key == K_RIGHT:
+                print('RIGHT')
+                testBlock.move('R',1*blockSize)
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
